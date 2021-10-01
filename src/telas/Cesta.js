ef7792bc28fbe069339ea6bd21f853f8955ec660
Ícaro Barboza <83622388+IcarusNB@users.noreply.github.com>
2021-10-01 12:18:12 -0300

    Initital Commit
    First steps on project initialization

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions, Text } from 'react-native';
+import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -9,6 +10,18 @@ export default function Cesta() {
     return <> 
         <Image source= {topo} style= {estilos.topo}/>
         <Text style= {estilos.titulo}>Detalhe da Cesta</Text>
+
+        <View style= {estilos.cesta}>
+            <Text style= {estilos.nome}>Cesta de Verduras</Text>
+
+            <View style= {estilos.fazenda}>
+                <Image source = {logo} style={estilos.imagemFazenda}/>
+                <Text style= {estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            </View>
+
+            <Text style= {estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha!</Text>
+            <Text style= {estilos.preco}>R$ 40,00</Text>
+        </View>
     </>
 }
 
@@ -28,5 +41,48 @@ const estilos = StyleSheet.create({
         lineHeight: 26,
         color: "white",
         fontWeight: "bold"
+    },
+
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+
+    nome: {
+        color: "#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontFamily: "MontserratBold"
+    },
+
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12
+    },
+
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
+
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: "MontserratRegular"
+    },
+
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 });
