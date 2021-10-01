65cec1e412306cb24a6242620c017b5ac1a438f9
Ícaro Barboza <83622388+IcarusNB@users.noreply.github.com>
2021-10-01 11:06:32 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
+
+import Cesta from './src/telas/Cesta';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+      </SafeAreaView>
+  );
+}
