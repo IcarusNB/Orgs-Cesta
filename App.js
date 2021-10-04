501e1bb636acb47c25de69dc67801de124fff8b9
Ícaro Barboza <83622388+IcarusNB@users.noreply.github.com>
2021-10-04 12:14:58 -0300

    Components
    Refactored application to add components

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { StatusBar, SafeAreaView, View } from 'react-native';
-import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 
 import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
   const [fonteCarregada] = useFonts({
     "MontserratRegular": Montserrat_400Regular,
-    "MontserratBold": Montserrat_700Bold
+    "MontserratBold": Montserrat_700Bold,
   });
 
   if (!fonteCarregada) {
-    return <View />
+    return <AppLoading />
   }
 
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
-      </SafeAreaView>
+      <Cesta {...mock} />
+    </SafeAreaView>
   );
 }
