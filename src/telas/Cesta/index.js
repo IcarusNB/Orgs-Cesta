6f8074c63bca575facb2e0a5cc3561bd06122f7d
Ícaro Barboza <83622388+IcarusNB@users.noreply.github.com>
2021-10-05 11:27:19 -0300

    List
    Added the FlatList to show the itens

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,22 +1,43 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 import Topo from './componentes/Topo';
 import Detalhes from './componentes/Detalhes';
+import Item from './componentes/Item';
+import Texto from '../../componentes/Texto';
 
-export default function Cesta({ topo, detalhes }) {
+export default function Cesta({ topo, detalhes,itens }) {
   return <>
-    <Topo {...topo} />
 
-    <View style={estilos.cesta}>
-      <Detalhes {...detalhes} />
-    </View>
+    <FlatList 
+      data= { itens.lista }
+      renderItem = { Item }
+      keyExtractor = {({ nome }) => nome } 
+
+      ListHeaderComponent={() => {
+        return <>
+          <Topo {...topo} />
+          <View style={estilos.cesta}>
+            <Detalhes {...detalhes} />
+            <Texto style={estilos.titulo}> {itens.titulo} </Texto>
+          </View>
+        </>
+      }}
+    />
   </>
 }
 
 const estilos = StyleSheet.create({
+  titulo: {
+    color: "#464646",
+    fontWeight:"bold",
+    marginTop: 32,
+    marginBottom: 8,
+    fontSize: 20,
+    lineHeight: 32,
+  },
   cesta: {
     paddingVertical: 8,
     paddingHorizontal: 16,
-  },
+  }
 });
